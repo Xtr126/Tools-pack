b0832daf3c794549e02512c259ef7245cec06b0a
Xtr126 <crawleymsd@gmail.com>
2021-05-13 13:31:16 +0530

    init

---
+++ gearlock/extension.sh
@@ -0,0 +1,67 @@
+#!/gearlock/bin/bash
+
+filesdir="$DEPDIR/toolx"
+
+function Lightning() {
+	HEIGHT=0
+	WIDTH=60
+	CHOICE_HEIGHT=0
+	BACKTITLE=$(gecpc "By Xtr" "+")
+	TITLE="Misc tools"
+	MENU="Misc tools"
+
+	OPTIONS=(1 "Find and batch install all apks in folder"
+	         2 "Extract gxp or other archive to RAM (faster)"
+	         3 "Backup directory with tar"
+	         4 "Make gxp which plays your own video and audio"
+			 5 "Change boot sound to your own from wav file"
+			 6 "Input device configurator"
+			 7 "Install zsh/fish/python shell to alt+f8 console"
+			 8 "Modify framework-res (ui icons)")
+
+	CHOICE=$(dialog --clear --cancel-label "Exit" \
+	                --backtitle "$BACKTITLE" \
+	                --menu "$MENU" \
+	                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+	                "${OPTIONS[@]}" \
+	                2>&1 >/dev/tty)
+
+    case $CHOICE in
+    	1)source $filesdir/apk;;
+		2)source $filesdir/egxp;;
+		3)source $filesdir/tar;;
+		4)source $filesdir/gxp;;
+		5)source $filesdir/bootsound;;
+		6)source $filesdir/idc;;
+		7)source $filesdir/zsh;;
+		8)source $filesdir/res;;
+		*);;
+	esac
+}
+
+
+function Loader() {
+
+PCT=0
+(
+while test $PCT != 105
+do
+cat <<EOF
+XXX
+$PCT
+$(figlet ToolPack)
+. . Made by Xtr  //
+XXX
+EOF
+PCT=`expr $PCT + 5`
+sleep 0.05
+done
+) |
+
+dialog "$@" --gauge "Hi, thanks" 12 45 0; sleep 0.5
+Lightning
+
+}
+
+
+Loader
---
+++ install.sh
@@ -0,0 +1,22 @@
+
+## For proper developer documentation, visit https://supreme-gamers.com/gearlock
+# Check `!zygote.sh` to configure your package functions or gearlock can also guide you during the build process
+
+#####--- Import Functions ---#####
+# get_base_dir # Returns execution directory path in $BD variable
+# get_net_stat
+check_compat 7.2.22
+#####--- Import Functions ---#####
+
+# Ensure compatible GearLock version
+
+geco "==== Cursor pack by Xtr, \n
+Special Thanks to my friends who supported in the creation of this
+Download previous version for boot animations, only cursors are in this, to make it lightweight
+NM AKSHAR Gave me 7 cursors
+Devplayz gave 6 cursors
+LIGHTNING283 gave me 4 cursors
+Developed with hours of patience by Xtr\n
+==== Use it from gearlock> run gearlock extensions
+==== Use it from gearlock extensions menu after installation, enjoy!"
+rm -rf $DEPDIR/cursorpack
