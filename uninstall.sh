e58e63f235c7cf90e4411b579cd2b8182c2af25b
Xtr126 <crawleymsd@gmail.com>
2021-05-21 22:39:41 +0530

    Update

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,1 @@
-filesdir="$DEPDIR/bootanimationchanger"
-backupdir="$STATDIR/${filesdir##*/}/backup"
-rm -f /system/media/bootanimation-after-first-boot.zip
-rsync -a "$backupdir/" /system/media
-chmod -f 644 /system/media/bootanimation-after-first-boot.zip
 
