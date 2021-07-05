78d83b05ffe9406505d82b5487cec526f8a80194
Xtr126 <crawleymsd@gmail.com>
2021-07-05 13:52:19 +0530

    update

--- gearlock/extension.sh
+++ gearlock/extension.sh
@@ -11,14 +11,15 @@ function Main_menu() {
 	MENU="Misc tools"
 
 	OPTIONS=(1 "Find and batch install all apks in folder"
-	         2 "Extract gxp/any archive to RAM(faster)"
+			 2 "Input device configurator"
 	         3 "Disable/enable boot animation"
-	         4 "Make gxp"
+	         4 "system settings changer"
 			 5 "Change boot sound(only .wav)"
-			 6 "Input device configurator"
+			 6 "Extract gxp/any archive to RAM(faster)"
 			 7 "Install zsh/fish/python shell to alt+f8 console"
 			 8 "Modify framework-res (ui icons)"
-			 9 "Free up some RAM")
+			 9 "Free up some RAM"
+			 10 "Make gxp")
 
 	CHOICE=$(dialog --clear --cancel-label "Exit" \
 	                --backtitle "$BACKTITLE" \
@@ -29,7 +30,7 @@ function Main_menu() {
 
     case $CHOICE in
     	1)source $filesdir/apk;;
-		2)source $filesdir/egxp;;
+		2)source $filesdir/idc;;
 		3)	
 			if (dialog --yes-label Disable --no-label Enable \
 			--yesno "Disables boot animation with build.prop line, useful for fast boot/ SSD" 7 45); then
@@ -38,14 +39,15 @@ function Main_menu() {
 				gearprop "debug.sf.nobootanimation 0"
             fi
 		;;
-		4)source $filesdir/gxp;;
+		4)source $filesdir/settings;;
 		5)source $filesdir/bootsound;;
-		6)source $filesdir/idc;;
+		6)source $filesdir/egxp;;
 		7)source $filesdir/zsh;;
 		8)source $filesdir/res;;
 		9)
 			echo 3 > /proc/sys/vm/drop_caches
 		;;
+		10)source $filesdir/gxp;;
 		*);;
 	esac
 }
@@ -61,7 +63,7 @@ cat <<EOF
 XXX
 $PCT
 $(figlet ToolPack)
-. . Made by Xtr  //
+. . . Made by Xtr  //
 XXX
 EOF
 PCT=`expr $PCT + 5`
@@ -69,7 +71,7 @@ sleep 0.05
 done
 ) |
 
-dialog "$@" --gauge "Hi, thanks" 12 45 0; sleep 0.5
+dialog --no-collapse --gauge "Hi, thanks" 12 45 0; sleep 0.5
 Main_menu
 
 }
