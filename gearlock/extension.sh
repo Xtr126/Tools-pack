a483178c86bd33e3c700a3c39179a480e01c57e7
Xtr126 <80520774+Xtr126@users.noreply.github.com>
2021-09-23 12:50:50 +0530

    Refactor, fix boot animation disable.

--- gearlock/extension.sh
+++ gearlock/extension.sh
@@ -2,7 +2,7 @@
 
 filesdir="$DEPDIR/toolx"
 
-function Main_menu() {
+Main_menu() {
 	HEIGHT=0
 	WIDTH=60
 	CHOICE_HEIGHT=0
@@ -11,15 +11,15 @@ function Main_menu() {
 	MENU="Misc tools"
 
 	OPTIONS=(1 "Find and batch install all apks in folder"
-			 2 "Input device configurator"
+		 2 "Input device configurator"
 	         3 "Disable/enable boot animation"
 	         4 "system settings changer"
-			 5 "Change boot sound(only .wav)"
-			 6 "Extract gxp/any archive to RAM(faster)"
-			 7 "Install zsh/fish/python shell to alt+f8 console"
-			 8 "Modify framework-res (ui icons)"
-			 9 "Free up some RAM"
-			 10 "Make gxp")
+		 5 "Change boot sound(only .wav)"
+		 6 "Extract gxp/any archive to RAM(faster)"
+		 7 "Install zsh/fish/python shell to alt+f8 console"
+		 8 "Modify framework-res (ui icons)"
+		 9 "Free up some RAM"
+		 10 "Make gxp")
 
 	CHOICE=$(dialog --clear --cancel-label "Exit" \
 	                --backtitle "$BACKTITLE" \
@@ -31,18 +31,13 @@ function Main_menu() {
     case $CHOICE in
     	1)source $filesdir/apk;;
 		2)source $filesdir/idc;;
-		3)	animation_on="
-		debug.sf.nobootanimation 0
-		"
-		animation_off="
-		debug.sf.nobootanimation 1
-		"
+		3)
 		
 			if (dialog --yes-label Disable --no-label Enable \
 			--yesno "Disables boot animation with build.prop line, useful for fast boot/ SSD" 7 45); then
-				gearprop "$animation_off"
+				update_value debug.sf.nobootanimation 1
 			else 
-				gearprop "$animation_on"
+				update_value debug.sf.nobootanimation 0
             fi
 		;;
 		4)source $filesdir/settings;;
@@ -59,34 +54,42 @@ function Main_menu() {
 			echo 3 > /proc/sys/vm/drop_caches
 		
 			geco "${GREEN}Memory usage after cleaning :${RC}"
-		free -h
+		geco "${CYAN}$(free -h)${RC}"
 		;;
 		10)source $filesdir/gxp;;
 		*);;
 	esac
 }
 
+update_value(){ 
 
-function Loader() {
+	if grep -q "$1=" "$SYSTEM_DIR"/build.prop; then
+		sed -i "s|$1=.*|$1=$2|g" "$SYSTEM_DIR"/build.prop
+	else
+		echo -e "$1=$2" >> "$SYSTEM_DIR"/build.prop
+	fi
 
-PCT=0
-(
-while test $PCT != 105
-do
-cat <<EOF
-XXX
-$PCT
-$(figlet ToolPack)
-. . . Made by Xtr  //
-XXX
-EOF
-PCT=`expr $PCT + 5`
-sleep 0.05
-done
-) |
+}
 
-dialog --no-collapse --gauge "Hi, thanks" 12 45 0; sleep 0.5
-Main_menu
+Loader() {
+
+	PCT=0
+	(
+	while test $PCT != 105;	do
+			cat <<EOF
+			XXX
+			$PCT
+			$(figlet ToolPack)
+			. . . Made by Xtr  //
+			XXX
+			EOF
+			PCT=`expr $PCT + 5`
+			sleep 0.05
+		done
+	) |
+
+	dialog --no-collapse --gauge "Hi, thanks" 12 45 0; sleep 0.5
+	Main_menu
 
 }
 
